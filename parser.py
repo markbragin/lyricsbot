78e9b2cc6c85022b53726faa597900acb1abf070
delbik <m4rk.brag1n@gmail.com>
2022-03-19 19:29:56 +0300

    added some typeckeckers

--- parser.py
+++ parser.py
@@ -3,7 +3,6 @@ import re
 
 import requests
 from bs4 import BeautifulSoup
-import sys
 
 
 def get_lyrics(songname: str) -> Optional[str]:
@@ -11,23 +10,30 @@ def get_lyrics(songname: str) -> Optional[str]:
     search_params = {"q": "+".join(songname.split() + ["genius.com"])}
 
     google_res = requests.get(search_url, search_params)
-    bs_search_page = BeautifulSoup(google_res.text, 'lxml')
+    if google_res.status_code != 200:
+        return None
 
+    search_page = BeautifulSoup(google_res.text, 'lxml')
 
-    a_tag = bs_search_page.find("a", {"href": re.compile("https://genius.com.")})
-    first_qlink = a_tag["href"]
+    a_tag = search_page.find("a", {"href": re.compile("https://genius.com.")})
+    if not a_tag:
+        return None
+    first_qlink = a_tag["href"]  # type: ignore
 
     genius_res = requests.get(f"https://google.com{first_qlink}")
+    if genius_res.status_code != 200:
+        return None
 
-    bs_genius_page = BeautifulSoup(genius_res.text.replace("<br/>", '\n'), 'lxml')
+    genius_page = BeautifulSoup(genius_res.text.replace("<br/>", '\n'), 'lxml')
+    lyrics_containers = genius_page.find_all("div",
+            {"data-lyrics-container": "true"})
 
-    lyrics_containers = bs_genius_page.find_all("div", {"data-lyrics-container": "true"})
     text = ""
     for container in lyrics_containers:
         text += container.get_text() + '\n'
 
-    return text
+    return text if text else None
 
 
 if __name__ == "__main__":
-    print(get_lyrics("loqiemean horovod"))
+    print(get_lyrics("kendrik lama dna"))
