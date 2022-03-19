675c8f1304c0cdde4f7d791a871c2a6d48bac101
delbik <m4rk.brag1n@gmail.com>
2022-03-19 19:19:11 +0300

    created raw genius lyrics parser

---
+++ parser.py
@@ -0,0 +1,33 @@
+from typing import Dict, Optional
+import re
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+
+def get_lyrics(songname: str) -> Optional[str]:
+    search_url = "https://google.com/search?"
+    search_params = {"q": "+".join(songname.split() + ["genius.com"])}
+
+    google_res = requests.get(search_url, search_params)
+    bs_search_page = BeautifulSoup(google_res.text, 'lxml')
+
+
+    a_tag = bs_search_page.find("a", {"href": re.compile("https://genius.com.")})
+    first_qlink = a_tag["href"]
+
+    genius_res = requests.get(f"https://google.com{first_qlink}")
+
+    bs_genius_page = BeautifulSoup(genius_res.text.replace("<br/>", '\n'), 'lxml')
+
+    lyrics_containers = bs_genius_page.find_all("div", {"data-lyrics-container": "true"})
+    text = ""
+    for container in lyrics_containers:
+        text += container.get_text() + '\n'
+
+    return text
+
+
+if __name__ == "__main__":
+    print(get_lyrics("loqiemean horovod"))
